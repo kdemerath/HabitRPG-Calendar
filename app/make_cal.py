de743046de08b550ce382bcd38e697c57988bad6
Klaus Demerath <kdemerath@gmx.de>
2016-10-11 09:39:14 +0200

    Reduce calendar to this month

--- app/make_cal.py
+++ app/make_cal.py
@@ -48,7 +48,7 @@ def get_display_dates():
 
 	display_dates = set()
 
-	for i in range(1, 13):
+	for i in range(1, datetime.datetime.now().month):
 		display_dates = display_dates.union(set(dates_for_month(this_year, i)))
 
 	display_dates = list(display_dates)
